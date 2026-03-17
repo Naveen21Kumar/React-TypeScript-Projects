fa002e3d8b83c46bfb372abfefac07d9537f9728
Naveen Kumar <kumar20maverick@gmail.com>
2026-03-17 21:14:54 +0530

    added styling in app.css and adding loading class

--- Mini/02_tours/src/Tour.tsx
+++ Mini/02_tours/src/Tour.tsx
@@ -4,13 +4,8 @@ const Tour = ({ id, name, info, image, price, removeTour }) => {
   const [readMore, setReadMore] = useState(false);
   return (
     <article className='single-tour'>
-      <img
-        src={image}
-        alt={name}
-        style={{ maxWidth: '400px' }}
-        className='img'
-      />
-      <span>{price}</span>
+      <img src={image} alt={name} className='img' />
+      <span className='tour-price'>{price}</span>
       <div className='tour-info'>
         <h5>{name}</h5>
         <p>
